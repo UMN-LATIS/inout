823e40235407a922823959df97beccff4cca4002
Colin McFadden <cmcfadden@gmail.com>
2019-08-27 21:10:36 -0500

    fix permissions for logs

--- deploy.php
+++ deploy.php
@@ -47,6 +47,13 @@ task('assets:generate', function() {
 })->desc('Assets generation');
 
 
+task('fix_storage_perms', '
+    touch storage/logs/laravel.log
+    sudo chown apache storage/logs/laravel.log
+    sudo chgrp apache storage/logs/laravel.log
+')->desc("Fix Apache Logs");
+after('artisan:migrate', 'fix_storage_perms');
+
 // $result = run("scl enable rh-php56 'php -v'");
 // Tasks
 
